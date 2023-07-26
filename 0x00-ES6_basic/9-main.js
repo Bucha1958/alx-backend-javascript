6d683d8849b4b37a7896998583e7c0f18554463f
Bucha1958 <okoriemmadu@gmail.com>
2023-07-26 07:30:12 -0700

    print the correct output

--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -1,6 +1,6 @@
 import getFullBudgetObject from "./9-getFullBudget.js";
 
-const fullBudget = getFullBudgetObject(400, 700, 900);
+const fullBudget = getFullBudgetObject(20, 50, 10);
 
-console.log(fullBudget.getIncomeInDollars(400));
-console.log(fullBudget.getIncomeInEuros(700));
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
