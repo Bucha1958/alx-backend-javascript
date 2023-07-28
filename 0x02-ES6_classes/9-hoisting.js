a6afe2c49a86fbf7fedc9a6aac0e70144e670972
Bucha1958 <okoriemmadu@gmail.com>
2023-07-28 07:00:42 -0700

    Create method that will clone current object

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable max-classes-per-file */
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -15,7 +17,6 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -32,7 +33,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    let self = this;
+    const self = this;
     return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
   }
 }
@@ -42,7 +43,6 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
