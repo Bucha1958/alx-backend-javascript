9f6b9fa106bb305082dea11b5d5c68eb7be79461
Bucha1958 <okoriemmadu@gmail.com>
2023-07-28 04:26:35 -0700

    implement inheritance

---
+++ 0x02-ES6_classes/6-main.js
@@ -0,0 +1,5 @@
+import SkyHighBuilding from "./6-sky_high.js";
+
+const buiding = new SkyHighBuilding(140, 60);
+console.log(buiding);
+console.log(buiding.evacuationWarningMessage());
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+// eslint-disable-next-line import/extensions
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
