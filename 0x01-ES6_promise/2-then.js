b852c69c411c3b3b250791095d91c3d997cdb048
Bucha1958 <okoriemmadu@gmail.com>
2023-07-29 08:43:19 -0700

    Include three handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-    // eslint-disable-next-line no-unused-vars
-    return promise.then((_value) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      promise
+        .then((resolve) => {
+          resolve({ status: 200, body: 'success' });
+        })
       // eslint-disable-next-line no-unused-vars
-    }).catch((_error) => {
-      console.log('Got a response from the API');
-      return new Error();
+        .catch((error) => new Error())
+        .finally(() => console.log('Got a response from the API'));
     });
+  });
 }
