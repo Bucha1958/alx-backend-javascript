63a96662db69e4ee5f5e4d8007b669a21671762d
Bucha1958 <okoriemmadu@gmail.com>
2023-07-30 06:06:43 -0700

    Correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars, import/no-unresolved, import/extensions
-import { createUser, uploadPhoto } from './utils';
+import { createUser, uploadPhoto } from '../0x01b_promise/utils.js';
 
-export default function handleProfileSignup() {
-  return Promise.all([createUser(), uploadPhoto()]).then((results) => {
-    const [userResult, photoResult] = results;
-    console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
-    // eslint-disable-next-line no-unused-vars
-  }).catch((error) => {
+export default async function handleProfileSignup() {
+  try {
+    const createUserProfile = await createUser();
+    const uploadPhotoProfile = await uploadPhoto();
+    console.log(`${uploadPhotoProfile.body} ${createUserProfile.firstName} ${createUserProfile.lastName}`);
+  } catch (error) {
     console.log('Signup system offline');
-  });
+  }
 }
