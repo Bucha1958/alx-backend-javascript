4692c64111e73db05a4aa2840ed7394671f5ae2e
Bucha1958 <okoriemmadu@gmail.com>
2023-08-01 10:13:44 -0700

    function that returns an array

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export default function getStudentsByLocation(getListStudents, city) {
+  const listStudents = getListStudents.filter((city) => city);
+  return listStudents;
+}
