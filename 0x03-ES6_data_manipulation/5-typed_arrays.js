1335ed9ad67389a94255723a9dc6148ba557810b
Bucha1958 <okoriemmadu@gmail.com>
2023-08-01 19:30:11 -0700

    Added for a test

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (length <= position) {
+      throw new Error('Position outside range');
+    }
+    const buffer = new ArrayBuffer(length);
+    const int8Array = new Int8Array(buffer);
+    const dataview = new DataView(buffer);
+    int8Array[position] = value;
+  
+    return dataview;
+  }
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+      return '';
+    }
+    const setArray = [...set];
+    const array = [];
+    setArray.forEach((value) => {
+      if (typeof value === 'string' && value.startsWith(startString)) {
+        const subStrVal = value.substring(startString.length);
+        if (subStrVal && subStrVal !== value) {
+          array.push(subStrVal);
+        }
+      }
+    });
+  
+    return array.join('-');
+  }
